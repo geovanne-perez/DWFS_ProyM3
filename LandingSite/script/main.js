48ad09c849879754cbf74d54288747f7a8a6cc7b
Geovanne Perez <geovanne.perez@outlook.com>
2024-01-20 19:44:56 -0600

    Estilos editados - 70% progreso
    with '#' will be ignored, and an empty message aborts the commit.

--- LandingSite/script/main.js
+++ LandingSite/script/main.js
@@ -1,8 +1,12 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {  
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+function toggleNav() {
+    if  (document.getElementById("mySidebar").style.width == "250px") {        
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+        document.getElementById("topheader").style.marginLeft= "0";
+    } 
+    else {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("topheader").style.marginLeft = "250px";
+    }
 }
